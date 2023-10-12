62b40adcdc0a8a2667d9bb4ebdce62a520f4ae9b
Jay <jligh43@wgu.edu>
2023-10-12 15:59:46 -0700

    Finished Task B

---
+++ src/main/java/com/d288/TravelAgencyProject/TravelAgencyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.d288.TravelAgencyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/d288/TravelAgencyProject/TravelAgencyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d288.TravelAgencyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
